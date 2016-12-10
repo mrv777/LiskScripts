8972a338107bbb1e91f5773ef06433245371a544
mrv777 <mrv777@users.noreply.github.com>
2016-12-09 21:49:39 -0600

    More aggressive setting

--- manage3.sh
+++ manage3.sh
@@ -145,10 +145,10 @@ while true; do
     if [[ "$FORGE1" == "true" ]];
     then
     	diff=$(( $HEIGHT - $HEIGHT1 ))
-    	if [ "$diff" -gt "3" ]
+    	if [ "$diff" -gt "2" ]
 		then
 			diff=$(( $HEIGHT - $HEIGHT2 ))
-			if [ "$diff" -gt "3" ]
+			if [ "$diff" -gt "2" ]
 			then
 				echo
 				echo "$SRV1 height too low. Switching to $SRV3"
@@ -167,10 +167,10 @@ while true; do
     if [[ "$FORGE2" == "true" ]];
     then
     	diff=$(( $HEIGHT - $HEIGHT2 ))
-    	if [ "$diff" -gt "3" ]
+    	if [ "$diff" -gt "2" ]
 		then
 			diff=$(( $HEIGHT - $HEIGHT3 ))
-			if [ "$diff" -gt "3" ]
+			if [ "$diff" -gt "2" ]
 			then
 				echo
 				echo "$SRV2 height too low. Switching to $SRV1"
@@ -189,10 +189,10 @@ while true; do
     if [[ "$FORGE3" == "true" ]];
     then
     	diff=$(( $HEIGHT - $HEIGHT3 ))
-    	if [ "$diff" -gt "3" ]
+    	if [ "$diff" -gt "2" ]
 		then
 			diff=$(( $HEIGHT - $HEIGHT1 ))
-			if [ "$diff" -gt "3" ]
+			if [ "$diff" -gt "2" ]
 			then
 				echo
 				echo "$SRV3 height too low. Switching to $SRV2"
