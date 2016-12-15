263b5253634017ebb9d50d750cc0bea94a1e475c
mrv777 <mrv777@users.noreply.github.com>
2016-12-15 13:32:12 -0600

    Updated log information

--- check_height_and_rebuild.sh
+++ check_height_and_rebuild.sh
@@ -131,9 +131,10 @@ local_height() {
 	if [ "$diff" -gt "4" ]
 	then
 		## Thank you doweig for better output formating
-        	date +"%Y-%m-%d %H:%M:%S || Reloading! Local: $CHECKSRV, Highest: $HEIGHT, Diff: $diff"
-		ChangeDirectory ## Make sure we are in the correct directory
-	##	bash lisk.sh reload  # 0.5.1 often solves short stucks by itself
+        	##date +"%Y-%m-%d %H:%M:%S || Reloading! Local: $CHECKSRV, Highest: $HEIGHT, Diff: $diff"
+		##ChangeDirectory ## Make sure we are in the correct directory
+		##bash lisk.sh reload  # 0.5.1 often solves short stucks by itself
+		date +"%Y-%m-%d %H:%M:%S || Sleeping for 140 seconds to wait for autocorrect! Local: $CHECKSRV, Highest: $HEIGHT, Diff: $diff"
 		sleep 140  #normally a short stuck is solved by itself in less then 140 seconds
 		
 		## Make sure local height is not empty, if it is empty try the call until it is not empty
