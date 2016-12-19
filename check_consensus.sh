8c1e672ec9385279c3605899afe9eb328b5816d0
mrv777 <mrv777@users.noreply.github.com>
2016-12-18 19:03:56 -0600

    Check other servers and switch before reload

--- check_consensus.sh
+++ check_consensus.sh
@@ -80,12 +80,37 @@ do
 		then
 			delegates=$(curl --connect-timeout 3 -s "http://"$SRV1""$PRT"/api/delegates/getNextForgers" | jq '.delegates')
 			date +"%Y-%m-%d %H:%M:%S || ${yellow}Low consensus.  Looking for delegate forging soon matching $pbk${resetColor}"
-			if [[ " ${delegates[@]} " =~ " ${pbk}" ]]; then
-				date +"%Y-%m-%d %H:%M:%S || ${red}You are forging soon, but your consensus is too low.  We will try a reload.${resetColor}"
-				ChangeDirectory
-				bash lisk.sh reload
-				sleep 20
-				SyncState
+			if [[ $delegates == *"$pbk"* ]];
+			then
+				date +"%Y-%m-%d %H:%M:%S || ${red}You are forging soon, but your consensus is too low. Looking to switch server before reload.${resetColor}"
+				for SERVER in ${SERVERS[@]}
+				do
+					## Get next server's height and consensus
+					SERVERINFO=$(curl --connect-timeout 3 -s -S "http://"$SERVER""$PRT"/api/loader/status/sync")
+					HEIGHT=$( echo "$SERVERINFO" | jq '.height')
+					CONSENSUS=$( echo "$SERVERINFO" | jq '.consensus')
+				
+					## Make sure next server is not more than 3 blocks behind this server and consensus is better, then switch
+					if [[  -n "$HEIGHT" ]];
+					then
+						diff=$(( $HEIGHTLOCAL - $HEIGHT ))
+					else
+						diff="999"
+					fi
+					## if [ "$diff" -lt "3" ] && [ "$CONSENSUS" -gt "$CONSENSUSLOCAL" ]; ## Removed for now as I believe consensus read from API isn't updated every second to be fully accurate
+					if [ "$diff" -lt "3" ]; 
+					then
+						curl -s -S --connect-timeout 3 -k -H "Content-Type: application/json" -X POST -d '{"secret":'"$SECRET"'}' https://"$SRV1""$PRTS"/api/delegates/forging/disable
+						curl -s -S --connect-timeout 3 -k -H "Content-Type: application/json" -X POST -d '{"secret":'"$SECRET"'}' https://"$SERVER""$PRTS"/api/delegates/forging/enable
+						echo
+						date +"%Y-%m-%d %H:%M:%S || ${cyan}Switching to Server $SERVER with a consensus of $CONSENSUS as your consensus is too low.  We will try a reload.${resetColor}"
+						ChangeDirectory
+						bash lisk.sh reload
+						sleep 20
+						SyncState
+						break
+					fi
+				done
 			fi
 		fi
 		
